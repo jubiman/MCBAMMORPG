67d3d3005c50d9f0fe864c1afad1e12528c447ed
Melle Bosboom <jubiman@users.noreply.github.com>
2021-12-23 20:17:58 +0100

    Initial commit 2.0

---
+++ src/main/java/com/jubiman/mcbammorpg/MCBAMMORPG.java
@@ -0,0 +1,10 @@
+package com.jubiman.mcbammorpg;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MCBAMMORPG implements ModInitializer {
+	@Override
+	public void onInitialize() {
+
+	}
+}
